9dae962b996d89645f4c8d342cffe21816edcdca
rosej904 <rosej904@gmail.com>
2024-03-28 20:55:38 -0400

    ticket-6-update database seed file - customers tabl

---
+++ db/index.js
@@ -0,0 +1,22 @@
+const { Client } = require('pg') // imports the pg module
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/planters-dev',
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
+
+/**
+ * customer Methods
+ */
+
+/**
+ * addresses Methods
+ */
+
+/**
+ * invetory Methods
+ */
+
+/**
+ * invetory_type Methods
+ */
