37ac87b90c995765500a70b5fa24d06594dd2178
rosej904 <rosej904@gmail.com>
2024-03-29 08:37:14 -0400

    minor: updated readMe

--- db/index.js
+++ db/index.js
@@ -2,7 +2,6 @@ const { Client } = require('pg') // imports the pg module
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/planters-dev',
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 });
 
 /**
