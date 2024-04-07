a72a85c7c2c2794ff22cd82d749463b489aafab3
AmiBray <gotstoread@gmail.com>
2024-04-07 06:42:48 -0400

    feat: add create inventory route

--- api/inventory.js
+++ api/inventory.js
@@ -6,6 +6,7 @@ const { getAllInventory,
     createInventory,
     updateInventory,
     deleteInventory } = require('../db/inventory');
+const { requireUser, RouteError } = require('./utils');
 
     //GET - /api/inventory - get all
     router.get('/', async (req, res, next) => {
@@ -16,3 +17,32 @@ const { getAllInventory,
             next(error);
         }
     });  
+
+    // GET - /api/inventory/:id - get a single product by id
+router.get('/:id', async (req, res, next) => {
+    try {
+        const productId = req.params.id;
+        const product = await getInventoryById(productId);
+        res.send(product);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// POST - /api/inventory - create a new product
+router.patch('/', requireUser, async (req, res, next) => {
+    if (req.user.role == "admin") {
+        try {
+            const product = await createInventory(req.body);
+            res.send(product);
+        } catch (error) {
+            next(error);
+        }
+    } else {
+        throw new RouteError({
+          status: 401,
+          name: "Unauthorized",
+          message: "You are not authorized to view all carts"
+        });
+    }
+});
