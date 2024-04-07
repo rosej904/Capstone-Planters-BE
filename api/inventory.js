23ec359779ca7637a12bd4e12f4e9246f98e7ff6
AmiBray <gotstoread@gmail.com>
2024-04-07 06:25:39 -0400

    feat: add get all and delete inventory

--- api/inventory.js
+++ api/inventory.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const router = express.Router();
+
+const { getAllInventory,
+    getInventoryById,
+    createInventory,
+    updateInventory,
+    deleteInventory } = require('../db/inventory');
+
+    //GET - /api/inventory - get all
+    router.get('/', async (req, res, next) => {
+        try {
+            const products = await getAllInventory();
+            res.send(products);
+        } catch (error) {
+            next(error);
+        }
+    });  
