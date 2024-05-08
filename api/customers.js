c67a8d058ac5d336ba52ba512085a1485816d30a
rosej904 <rosej904@gmail.com>
2024-05-08 19:05:38 -0400

    deploy

--- api/customers.js
+++ api/customers.js
@@ -99,7 +99,7 @@ customersRouter.post('/logout', async (req, res, next) => {
     }else{
       console.log("clearing cookie")
       // res.cookie("jwtCust", "null", {path:"/"})
-      res.cookie('jwtCust', 'null', {path:'/',sameSite:'none',httpOnly:'true', secure:'true', maxAge: 0})
+      res.cookie('jwtCust', 'null', {path:'/', domain:'.onrender.com', sameSite:'none',httpOnly:'true', secure:'true', maxAge: 0})
       res.sendStatus(204)
     
     }
