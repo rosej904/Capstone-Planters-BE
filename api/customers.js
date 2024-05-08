3f2360f04c40fc77a7665a17b1479f4e2f7a2dc0
rosej904 <rosej904@gmail.com>
2024-05-08 17:18:33 -0400

    testing deploy

--- api/customers.js
+++ api/customers.js
@@ -98,7 +98,7 @@ customersRouter.post('/logout', async (req, res, next) => {
       res.send({name: "NoAuth", message:"NoAuth"})
     }else{
       console.log("clearing cookie")
-      res.cookie("jwtCust", "null", {path:"/"})
+      // res.cookie("jwtCust", "null", {path:"/"})
       res.clearCookie("jwtCust",{path:"/", domain:".onrender.com"})
     return res.sendStatus(204)
     }
